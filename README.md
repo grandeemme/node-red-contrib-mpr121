cf3769f49bf78f55ed615352bcdd00c46854624b
Miranda Mauro <grande.emme@gmail.com>
2015-12-29 17:13:50 +0100

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # node-red-contrib-mpr121
-Node for interacting with MPR121 with node red 
+Node for interacting with MPR121 
---
+++ mpr121/mpr121.js
@@ -0,0 +1,28 @@
+var Mpr121 = require('mpr121.js');
+
+module.exports = function(RED) {
+	function Mpr121InterruptNode(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+
+		// Address, I2c Bus, Gipio interrupt
+		var mod = new Mpr121(0x5A, 1);
+
+		mod.notifyTouch = function(pin) {
+			var msg = {
+				payload : pin
+			}
+			node.send(msg);
+		}
+
+		mod.notifyRelease = function(pin) {
+			var msg = {
+				payload : pin
+			}
+			node.send(msg);
+		}
+
+		mod.startInterrupt(4);
+	}
+	RED.nodes.registerType("mpr121-interrupt", Mpr121InterruptNode);
+}
